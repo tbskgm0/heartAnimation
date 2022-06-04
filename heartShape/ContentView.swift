539a39cd715b800a57b8854a38ba7f37f0cd4bdc
tbskgm0 <tsubasa.kogoma@axia-works.com>
2022-06-04 15:38:39 +0900

    Initial Commit

---
+++ heartShape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  heartShape
+//
+//  Created by tsubasa.kogoma on 2022/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ heartShape/heartShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartShapeApp.swift
+//  heartShape
+//
+//  Created by tsubasa.kogoma on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct heartShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
